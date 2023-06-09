0114fc9d0520cf1f1981c52e06276bd1dc5aa79c
gyahury <rudgns1243@naver.com>
2023-06-09 23:12:30 +0900

    [RB-7] Chore: ico 변경

--- src/app/components/navbar/Logo.tsx
+++ src/app/components/navbar/Logo.tsx
@@ -12,7 +12,7 @@ const Logo = () => {
       className="hidden cursor-pointer md:block"
       height="100"
       width="100"
-      src="/images/logo3.png"
+      src="/images/logo.png"
       priority
     />
   );
