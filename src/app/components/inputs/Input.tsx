d4f36f3a892f248ccb00c5b67325006b7e535b61
gyahury <rudgns1243@naver.com>
2023-06-26 18:03:30 +0900

    [RB-22] Chore: Input 컴포넌트 수정

--- src/app/components/inputs/Input.tsx
+++ src/app/components/inputs/Input.tsx
@@ -36,7 +36,7 @@ const Input: React.FC<InputProps> = ({
         id={id}
         disabled={disabled}
         {...register(id, { required })}
-        placeholder=""
+        placeholder=" "
         type={type}
         className={`peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none transition disalbed:opacity-70 disabled:cursor-not-allowed
             ${formatPrice ? "pl-9" : "pl-4"}
@@ -45,12 +45,8 @@ const Input: React.FC<InputProps> = ({
             `}
       />
       <label
-        className={`absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0]
+        className={`absolute text-md scale-75 duration-300 transform -translate-y-3 top-5 z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3
           ${formatPrice ? 'left-9' : 'left-4'}
-          peer-placeholder-shown:scale-100
-          peer-placeholder-shown:translate-y-0
-          peer-focus:scale-75
-          peer-focus:-translate-y-4
           ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}
         `}
       >
