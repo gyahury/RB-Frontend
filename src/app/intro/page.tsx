0d81934512505731fbed22cac39aad4cf93b963f
gyahury <rudgns1243@naver.com>
2023-08-09 00:25:07 +0900

    [RB-43] Chore: framer-motion 추가

--- src/app/intro/page.tsx
+++ src/app/intro/page.tsx
@@ -1,11 +1,23 @@
+"use client";
+
 import Container from "../components/Container";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Intro() {
-    return (
-      <Container>
-        <div className="pt-24 text-center">
-            <div>부동산 매물을 확인해보세요.</div>
-        </div>
-      </Container>
-    )
-  }
+  return (
+    <Container>
+      <div className="pt-24 text-center">
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            부동산 매물을 확인해보세요.
+          </motion.div>
+        </AnimatePresence>
+      </div>
+    </Container>
+  );
+}
